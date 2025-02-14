3da2dc8287a8c10c5ad4ed8a169b4bf82ad2ade6
serge <joe.r.suliman@gmail.com>
2025-02-14 16:48:00 +0100

    4

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,32 @@ window.addEventListener('scroll', () =>{
     }
 });
 
+
+// ------------------------------------DARK&LIGHT-----------------------------------------
+
+
+const themeStylesheet = document.getElementById('theme-stylesheet');
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+// On vérifie si le mode dark est activé dans le localStorage et on applique le thème approprié
+if (localStorage.getItem('theme') === 'light') {
+    themeStylesheet.setAttribute('href', 'assets/style/light-mode.css');
+    darkModeToggle.checked = true; // On coche la case si le mode clair est activé
+}
+
+// Écouteur d'événements pour le changement d'état de la checkbox
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        // Si la case est cochée, on applique le mode clair
+        themeStylesheet.setAttribute('href', 'assets/style/light-mode.css');
+        
+        localStorage.setItem('theme', 'light');
+    } else {
+        // Si la case n'est pas cochée, on applique le mode sombre
+        themeStylesheet.setAttribute('href', 'assets/style/style.css');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+
+
